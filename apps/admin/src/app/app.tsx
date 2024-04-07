e45073dbfea1b332c1f0dd35924d86870e72638d
Milosz Bukala <milosz.bukala@giraffestudio.pl>
2024-04-07 09:19:13 +0200

    feat: add railway files and error components

--- apps/admin/src/app/app.tsx
+++ apps/admin/src/app/app.tsx
@@ -10,6 +10,7 @@ import { far } from '@fortawesome/free-regular-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { ReviewsPage } from './pages/ReviewsPage';
+import { ErrorBoundary } from './components/ErrorBoundary';
 
 library.add(far, fas, fab);
 
@@ -17,46 +18,48 @@ const queryClient = new QueryClient();
 
 export function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <div>
-        <div className="bg-slate-900 text-white py-2">
-          <div className="container mx-auto py-2">
-            <div role="navigation">
-              <Menu>
-                <MenuItem>
-                  <Link to="/">Home</Link>
-                </MenuItem>
+    <ErrorBoundary>
+      <QueryClientProvider client={queryClient}>
+        <div>
+          <div className="bg-slate-900 text-white py-2">
+            <div className="container mx-auto py-2">
+              <div role="navigation">
+                <Menu>
+                  <MenuItem>
+                    <Link to="/">Home</Link>
+                  </MenuItem>
 
-                <MenuItem>
-                  <Link to="/reviews">Reviews</Link>
-                </MenuItem>
-              </Menu>
+                  <MenuItem>
+                    <Link to="/reviews">Reviews</Link>
+                  </MenuItem>
+                </Menu>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex flex-col h-screen">
+            <div className="w-full mx-auto">
+              <Routes>
+                <Route path="/" element={<DashboardPage />} />
+
+                <Route path="/reviews" element={<ReviewsPage />} />
+
+                <Route
+                  path="/page-2"
+                  element={
+                    <div>
+                      <Link to="/">Click here to go back to root page.</Link>
+                    </div>
+                  }
+                />
+              </Routes>
             </div>
           </div>
         </div>
 
-        <div className="flex flex-col h-screen">
-          <div className="w-full mx-auto">
-            <Routes>
-              <Route path="/" element={<DashboardPage />} />
-
-              <Route path="/reviews" element={<ReviewsPage />} />
-
-              <Route
-                path="/page-2"
-                element={
-                  <div>
-                    <Link to="/">Click here to go back to root page.</Link>
-                  </div>
-                }
-              />
-            </Routes>
-          </div>
-        </div>
-      </div>
-
-      <ReactQueryDevtools initialIsOpen={true} />
-    </QueryClientProvider>
+        <ReactQueryDevtools initialIsOpen={true} />
+      </QueryClientProvider>
+    </ErrorBoundary>
   );
 }
 
